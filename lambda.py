8362378f73af7e0ac0b8208b8d3b3476b64f3ea4
elams18 <30745311+elams18@users.noreply.github.com>
2021-12-03 16:31:28 +0530

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -9,10 +9,12 @@ log = logging.getLogger(__name__)
 
 s3 = boto3.client('s3')
 S3_BUCKET = "imagesbewelldigital"
-S3_BASE_URL = "https://imagesbewelldigital.s3.ap-south-1.amazonaws.com/"
 
 def lambda_handler(event, context):
+    REGION = event['Records'][0]['awsRegion']
+    S3_BASE_URL = f"https://{S3_BUCKET}.s3.{REGION}.amazonaws.com/"
     log.info('Loading function')
+    
     try:
         log.info(f'Calling out to {S3_BUCKET} bucket to list objects')
         images = s3.list_objects(Bucket=S3_BUCKET, MaxKeys=10)
