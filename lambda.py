5199969b57165ddb0d52fa4e6ca67dffa7c80721
elams18 <30745311+elams18@users.noreply.github.com>
2021-12-03 16:17:17 +0530

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -1,3 +1,4 @@
+
 import json
 import urllib.parse
 import boto3
@@ -8,6 +9,7 @@ log = logging.getLogger(__name__)
 
 s3 = boto3.client('s3')
 S3_BUCKET = "imagesbewelldigital"
+S3_BASE_URL = "https://imagesbewelldigital.s3.ap-south-1.amazonaws.com/"
 
 def lambda_handler(event, context):
     log.info('Loading function')
@@ -17,7 +19,7 @@ def lambda_handler(event, context):
         response = dict()
         response['response'] = []
         for image in images['Contents']:
-            response['response'].append("https://imagesbewelldigital.s3.ap-south-1.amazonaws.com/"+image['Key'])
+            response['response'].append(S3_BASE_URL+image['Key'])
     except Exception as e:
         log.info(e)
         log.info(f'Error getting from bucket {S3_BUCKET}. Make sure they exist and your bucket is in the same region as this function.')
